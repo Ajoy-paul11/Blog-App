36d7a7b6b53ad5e0147f99e9ad7d1ac06ce6b815
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-12 23:58:55 +0530

    "Update CSS classes for post layout and padding in Home and Post components"

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -36,7 +36,7 @@ export default function Post() {
   };
 
   return post ? (
-    <div className="py-8 ">
+    <div className="py-8 px-2">
       <Container>
         <div className="w-full flex justify-center mb-4 relative border-none rounded-xl p-2 ring-4 ring-[#00FFF5]/60">
           <img
